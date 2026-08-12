f43c40ccf01be78e24a88dd358bd17222a61aeb1
岡崎陽平 <rain1171110>
2026-08-12 10:35:41 +0900

    JWTログインとCookie設定を追加

--- server/index.ts
+++ server/index.ts
@@ -33,7 +33,7 @@ const isPrismaError = (error: unknown, code: PrismaErrorCode): boolean => {
   );
 };
 
-app.use(cors());
+app.use(cors({ origin: "http://" + "localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 

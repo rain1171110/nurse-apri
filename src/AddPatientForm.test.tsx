79f9613d01f7d5afdf81b585d05b219536c58f56
岡崎陽平 <rain1171110>
2026-09-24 16:46:09 +0900

    test: cover AddPatientForm visibility and cancel

--- src/AddPatientForm.test.tsx
+++ src/AddPatientForm.test.tsx
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 import { beforeEach, afterEach, describe, expect, it, vi } from "vitest";
 import AddPatientForm from "./AddPatientForm";
+import userEvent from "@testing-library/user-event";
 
 describe("AddPatientForm", () => {
   beforeEach(() => {
@@ -24,8 +25,73 @@ describe("AddPatientForm", () => {
         setShowAddForm={vi.fn()}
       />,
     );
+    expect(
+      screen.queryByRole("heading", { name: "患者を追加" }),
+    ).not.toBeInTheDocument();
   });
-  expect(
-    screen.queryByRole("heading", { name: "患者を追加" }),
-  ).not.toBeInTheDocument();
+
+  it("showAddFormがtrueならフォームを表示する", () => {
+    const onSubmit = vi.fn();
+
+    render(
+      <AddPatientForm
+        patients={[]}
+        onSubmit={onSubmit}
+        showAddForm={true}
+        setShowAddForm={vi.fn()}
+      />,
+    );
+    expect(
+      screen.getByRole("heading", { name: "患者を追加" }),
+    ).toBeInTheDocument();
+  });
+
+  it("「キャンセル」を押すとフォームを閉じる処理のテスト", async () => {
+    const onSubmit = vi.fn();
+    const setShowAddForm = vi.fn();
+
+    const user = userEvent.setup();
+
+    render(
+      <AddPatientForm
+        patients={[]}
+        onSubmit={onSubmit}
+        showAddForm={true}
+        setShowAddForm={setShowAddForm}
+      />,
+    );
+    const cancelButton = screen.getByRole("button", {
+      name: "キャンセル",
+    });
+    await user.click(cancelButton);
+
+    expect(setShowAddForm).toHaveBeenCalledWith(false);
+  });
+
+  it("「保存」を押すと、onSubmitに入力値が渡されるか", async () => {
+    const onSubmit = vi.fn();
+
+    const user = userEvent.setup();
+
+    render(
+      <AddPatientForm
+        patients={[]}
+        onSubmit={onSubmit}
+        showAddForm={showAddForm}
+        setShowAddForm={vi.fn()}
+      />,
+    );
+
+    const nameInput = await screen.findByLabelText("氏名");
+    const roomInput = await screen.findByLabelText("部屋番号");
+    await user.type(nameInput, "testName");
+    await user.type(roomInput, "999");
+
+    const saveButton = screen.getByRole("button", {
+      name: "保存",
+    });
+    await user.click(saveButton);
+
+    expect(showAddForm).toHaveBeenCalledWith(true);
+  });
 });
